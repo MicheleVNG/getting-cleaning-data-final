76fbe03f73c200ed331da17a6fe605f56f020267
Michele Vangelista <m.vangelista@gmail.com>
2017-01-06 19:30:57 +0100

    First complete version of run_analysis.R, not fully tested

--- run_analysis.R
+++ run_analysis.R
@@ -8,6 +8,7 @@
 #    the average of each variable for each activity and each subject.
 
 library(readr)
+library(plyr)
 library(dplyr)
 
 # Change the working directory to the 'data' directory
@@ -17,8 +18,8 @@ setwd(file.path(basewd, "data"))
 # Read activity labels and features labels, then name the columns
 activity_labels <- read.table("activity_labels.txt")
 features <- read.table("features.txt")
-colnames(activity_labels) <- c("id", "ActivityLabel")
-colnames(features) <- c("id", "FeatureName")
+colnames(activity_labels) <- c("activityId", "activityLabel")
+colnames(features) <- c("featureId", "featureName")
 
 # Read train and test files
 x_train 	<- read.table(file.path("train", "X_train.txt"), colClasses = "numeric")
@@ -33,21 +34,40 @@ all_measurements <- bind_rows(x_train, x_test)
 all_activities <- bind_rows(y_train, y_test)
 all_subjects <- bind_rows(subject_train, subject_test)
 
+# Clear unnecessary variables
+rm(list = c("x_train", "y_train", "subject_train", "x_test", "y_test", "subject_test"))
+
 # Name the columns in the three data frames
-colnames(all_measurements) <- features$FeatureName
+colnames(all_measurements) <- features$featureName
 colnames(all_activities) <- "activityId"
 colnames(all_subjects) <- "subjectId"
+
+# Add labels to the all_actvities data frame
+all_activities <- join(all_activities, activity_labels, by = "activityId")
 
 # Filter out measurements that are not mean() or std()
 all_measurements <- all_measurements[, grep("mean()|std()", colnames(all_measurements))]
 all_measurements <- all_measurements[, -grep("meanFreq()", colnames(all_measurements))]
 
+# Merge everything in one dataset, and clean it up
+dataset <- bind_cols(all_subjects, all_activities, all_measurements)
+dataset <- dataset %>%
+	select(-activityId) %>%
+	dplyr::rename(subject = subjectId, activity = activityLabel)
 
-# NOW NEED TO MERGE TABLES INTO ONE DATASET
+# Hypothesis: rename columns using descriptive names
+# newnames <- colnames(dataset)
+# newnames <- gsub("-mean[(][)]-", "MeanOn", newnames)
+# newnames <- gsub("-mean[(][)]", "Mean", newnames)
+# newnames <- gsub("-std[(][)]-", "StandardDeviationOn", newnames)
+# newnames <- gsub("-std[(][)]", "StandardDeviation", newnames)
 
+# Hypothesis: 'tall' version of the same data
+# molten_dataset <- melt(dataset, id = c("subject", "activity"))
 
-# dataset <- bind_cols(x_filtered, y_all, subject_all)
-# colnames(dataset)
-
-
+# Second summary dataset with means
+summary_dataset <- dataset %>%
+	tbl_df %>% 
+	group_by(subject, activity) %>%
+	summarize_each(funs(mean))
 
